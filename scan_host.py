9cb72b0a51fb4865c0587b9e834a6d5fbbbda252
Rocky Chen <Rockyzsu@users.noreply.github.com>
2016-05-15 21:53:15 +0800

    Update scan_host.py

--- scan_host.py
+++ scan_host.py
@@ -3,9 +3,9 @@ import time
 from thread_test import MyThread
 
 socket.setdefaulttimeout(1)
-#设置每个线程socket的timeou时间，超过1秒没有反应就认为端口不开放
+
 thread_num=4
-#线程数目 
+ 
 ip_end=256
 ip_start=0
 scope=ip_end/thread_num
@@ -17,7 +17,7 @@ def scan(ip_head,ip_low, port):
 	print ip
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((ip, port))
-	#通过这一句判断 是否连通
+	
         s.close()
 	print "ip %s port %d open\n" %(ip,port)
         return True
@@ -51,6 +51,6 @@ for i in range(thread_num):
 	threads[i].start()
 for i in range(thread_num):
 	threads[i].join()
-	#设置进程阻塞，防止主线程退出了，其他的多线程还在运行
+	
 
 print "*****end*****"
